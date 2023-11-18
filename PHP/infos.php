8b117751eb47ef203e780b89205aadad6e0c831d
AdmirMedeiros <101841196+AdmirMedeiros@users.noreply.github.com>
2023-11-18 10:10:02 -0300

    Create infos.php

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "db-admir.cnrbxfikli0f.sa-east-1.rds.amazonaws.com";
+$username = "admir";
+$password = "qOeki3fLqEkjMmHoIZI6";
+$database = "db-loja";
+?>
